28da37accd27a04bb71ff373ff3d58730caa15f6
NoreenNaz1234 <38943389+NoreenNaz1234@users.noreply.github.com>
2018-09-30 17:54:29 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# my-chart
+# Charts.js
+
+Chart.js is a JavaScript library that allows you to draw different types of charts by using the HTML5 canvas element. Since it uses canvas 
+,you have to include a polyfill to support older browsers.
+
+## Getting Started
+
+First you need CDN to get chart library
+
+For getting CND you have to go the website 
+
+This is the website where you have to go and click the get started button https://www.chartjs.org/
+
+### My work is depend on population 
+
+It is hard coded 
+
+It tells about population of five country
+
+```
+Example. country: Asia, population: 43000
+```
+
+### You can see my work here
+
+https://noreennaz1234.github.io/chart-sheet/
+
---
+++ script.js
@@ -0,0 +1,59 @@
+function intial(){
+	 fetchData();
+
+let tableEl = document.getElementById("table")
+
+
+let data = {labels: [], population: []}
+
+function fetchData() {
+
+
+$.ajax({
+		url: `http://api.population.io:80/1.0/population/1980/aged/18/`,
+		success: function (apidata) {
+			
+
+			for(let i = 0; i < 5; i++){
+
+			data.labels.push(apidata[i].country);
+			data.population.push(apidata[i].total);
+			// console.log(data.labels.push(apidata[i].country))
+			// console.log(data.population.push(apidata[i].total))
+
+
+			}
+
+
+   
+	new Chart(document.getElementById("doughnut-chart"), {
+  
+		type: 'doughnut',
+		data: {
+			labels: data.labels,
+			datasets: [
+			{
+				label: "Population (millions)",
+				backgroundColor: ["#f37123", "#0fad00","#a44599","#d20264","#C5007D"],
+				data: data.population
+			}
+			]
+		},
+		options: {
+			title: {
+				display: true,
+				text: 'Predicted world population (millions) in 2050'
+			}
+		}
+	});
+
+
+		},
+		error: function (err) {
+			// alert();
+		}
+	});
+}
+
+
+}
